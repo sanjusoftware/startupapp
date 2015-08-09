dc4fd7ebedbec6be158058b983a20878483679ab
Sanjeev Mishra <sanjusoftware@gmail.com>
2015-08-10 01:08:34 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shrota::Application.config.secret_key_base = 'b584db2558b45d35e447d21fbeaa361450ea24ed9808deebdddd539d349a5d2cb7b355abf1e071cc10c37ce9dece0a3653f299a79ae6328aef552d10e261a375'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Shrota::Application.config.session_store :cookie_store, key: '_shrota_session'
