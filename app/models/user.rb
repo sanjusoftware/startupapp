fdf2e51dcc58bc2872245607425072c2157f91ce
Sanjeev Mishra <sanjusoftware@gmail.com>
2015-08-11 19:34:56 +0530

    displaying devise error messages to the forms and changed the routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
+
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
+  end
 end
